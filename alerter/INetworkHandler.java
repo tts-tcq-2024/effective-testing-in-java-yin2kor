3367746b9879b4b8bf4e94d598afb9d444250cdf
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-08-07 23:04:28 +0530

    moved all into single file since imports are not working

--- alerter/INetworkHandler.java
+++
@@ -1,3 +0,0 @@
-public interface INetworkHandler {
-    int networkAlert(float celcius);
-}
--- alerter/alerter.java
+++ alerter/alerter.java
@@ -22,3 +22,18 @@ public class alerter {
         System.out.println("All is well (maybe!)\n");
     }
 }
+
+interface INetworkHandler {
+    int networkAlert(float celcius);
+}
+
+class NetworkHandlerStub implements INetworkHandler{
+    @Override
+    public int networkAlert(float celcius) {
+        System.out.println("ALERT: Temperature is " + celcius + " celcius");
+        // Return 200 for ok
+        // Return 500 for not-ok
+        // stub always succeeds and returns 200
+        return 500;
+    }
+}
