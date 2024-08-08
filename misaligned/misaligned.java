2c617a2e2b2a3cfc738b5d8123c58c0cadeb53e7
Venugopal K <venu.shastri@gmail.com>
2024-08-08 15:29:27 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -7,7 +7,7 @@ public class misaligned {
         int i = 0, j = 0;
         for (i = 0; i < 5; i++) {
             for (j = 0; j < 5; j++) {
-                System.out.printf(getColorComboInfo(i, j));
+                System.out.printf(getColorComboInfo(i, i));
             }
         }
         return i * j;
